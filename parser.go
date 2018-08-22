084fa1a2b8c87781ff9e8598eb7b43c6cefc6003
Komei Nomura <komei.nomura@pepabo.com>
2018-08-22 19:32:09 +0900

    Cのパースライブラリをpackage化
    golangのpackageとして利用可能とするため

---
+++ parser.go
@@ -0,0 +1,26 @@
+package parser
+
+import (
+// #cgo CFLAGS: -I./lib/c_tokenizer.h
+// #include "./C/c_tokenizer.c"
+	"C"
+	"unsafe"
+)
+
+type Parser struct {}
+
+func (p Parser) Parse(query string) string {
+	query_length := C.int(len(query))
+	query_c := C.CString(query)
+	var first_comment **C.char = nil
+	var buf *C.char = nil
+
+	query_digest_c := C.mysql_query_digest_and_first_comment(query_c, query_length, first_comment, buf)
+	query_digest := C.GoString(query_digest_c)
+
+	C.free(unsafe.Pointer(query_c))
+	C.free(unsafe.Pointer(first_comment))
+	C.free(unsafe.Pointer(buf))
+
+	return query_digest
+}
